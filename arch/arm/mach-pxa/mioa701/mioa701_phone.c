d1d5642c0f7ddf285e8de9c66848599385e049e0
Robert Jarzmik <rjarzmik@free.fr>
2008-02-08 00:05:49 +0100

    Improved phone initialization after wince dissassembly.
    Disassembly of mux_07_10.dll, and XS200 pinout specification
    found.

--- arch/arm/mach-pxa/mioa701/mioa701_phone.c
+++ arch/arm/mach-pxa/mioa701/mioa701_phone.c
@@ -9,6 +9,8 @@
 #include <linux/kernel.h>
 #include <linux/delay.h>
 #include <linux/platform_device.h>
+#include <linux/interrupt.h>
+#include <linux/irq.h>
 
 #include <asm/gpio.h>
 #include <asm/hardware.h>
@@ -17,22 +19,78 @@
 #include "mioa701.h"
 
 #define PWER_GPIO113 (0x2 << 19)
+#define OUTLO (GPIO_OUT | GPIO_DFLT_LOW)
+#define OUTHI (GPIO_OUT | GPIO_DFLT_HIGH)
 
 extern struct platform_pxa_serial_funcs mioa701_phone_funcs;
+
+static int is_phone_on(void)
+{
+	return (gpio_get_value(MIO_GPIO_GSM_MOD_ON_STATE) != 0);
+}
+
+irqreturn_t phone_power_irq(int irq, void* dev_id, struct pt_regs *regs)
+{
+	printk("Mioa701: GSM power changed to %s\n",
+	       is_phone_on() ? "on" : "off");
+	return IRQ_HANDLED;
+}
+
+static void phone_init_gpios(void)
+{
+	/* configure XS200 GPIOS */
+	pxa_gpio_mode(MIO_GPIO_GSM_nMOD_ON_CMD         | OUTHI);
+	pxa_gpio_mode(MIO_GPIO_GSM_nMOD_OFF_CMD        | OUTHI);
+	pxa_gpio_mode(MIO_GPIO_GSM_nMOD_DTE_UART_STATE | OUTHI);
+	pxa_gpio_mode(MIO_GPIO_GSM_MOD_RESET_CMD       | OUTLO);
+
+	pxa_gpio_mode(MIO_GPIO_GSM_EVENT | GPIO_IN);
+}
+
+static void ffuart_init_gpios(void)
+{
+	pxa_gpio_mode(MIO_GPIO_GSM_RXD_MD);
+	pxa_gpio_mode(MIO_GPIO_GSM_CTS_MD);
+	pxa_gpio_mode(MIO_GPIO_GSM_DCD_MD);
+	pxa_gpio_mode(MIO_GPIO_GSM_DSR_MD);
+	pxa_gpio_mode(MIO_GPIO_GSM_TXD_MD);
+	pxa_gpio_mode(MIO_GPIO_GSM_DTR_MD);
+	pxa_gpio_mode(MIO_GPIO_GSM_RTS_MD);
+}
+
+static void phone_reset(void)
+{
+	phone_init_gpios();
+	msleep(20);
+	gpio_set_value(MIO_GPIO_GSM_MOD_RESET_CMD, 1);
+	msleep(110);
+	gpio_set_value(MIO_GPIO_GSM_MOD_RESET_CMD, 0);
+}
+
+static void phone_off(void)
+{
+	gpio_set_value(MIO_GPIO_GSM_nMOD_DTE_UART_STATE, 0);
+	msleep(110);
+	gpio_set_value(MIO_GPIO_GSM_nMOD_DTE_UART_STATE, 1);
+	gpio_set_value(MIO_GPIO_GSM_nMOD_OFF_CMD, 0);
+	msleep(110);
+	gpio_set_value(MIO_GPIO_GSM_nMOD_OFF_CMD, 1);
+	msleep(110);
+	printk("MioA701 phone: GSM turned off.\n");
+}
 
 static void phone_on(void)
 {
 	int tries;
 
-	/* pre-serial-up hardware configuration */
-	gpio_set_value(MIO_GPIO_GSM_UNKNOWN2, 1);
-	gpio_set_value(MIO_GPIO_GSM_UNKNOWN1, 1);
-	gpio_set_value(MIO_GPIO_GSM_PREP, 1);
-	gpio_set_value(MIO_GPIO_GSM_OFF, 0);
-	msleep(100);
-	gpio_set_value(MIO_GPIO_GSM_PREP, 0);
-	msleep(10);
-	gpio_set_value(MIO_GPIO_GSM_PREP, 1);
+	phone_init_gpios();
+	if (is_phone_on())
+		phone_off();
+	phone_reset();
+	msleep(300);
+	gpio_set_value(MIO_GPIO_GSM_nMOD_ON_CMD, 0);
+	msleep(110);
+	gpio_set_value(MIO_GPIO_GSM_nMOD_ON_CMD, 1);
 
 	tries = 0;
 	do {
@@ -43,22 +101,6 @@ static void phone_on(void)
 	} else {
 		printk("MioA701 phone: GSM turned on in %d ms\n", tries*50);
 	}
-}
-
-static void phone_off(void)
-{
-	gpio_set_value(MIO_GPIO_GSM_UNKNOWN1, 1);
-	msleep(1);
-	gpio_set_value(MIO_GPIO_GSM_PREP, 1);
-	msleep(1);
-	gpio_set_value(MIO_GPIO_GSM_OFF, 1);
-	gpio_set_value(MIO_GPIO_GSM_UNKNOWN1, 0);
-	msleep(1);
-	gpio_set_value(MIO_GPIO_GSM_UNKNOWN1, 1);
-	gpio_set_value(MIO_GPIO_GSM_UNKNOWN2, 0);
-	msleep(1);
-	gpio_set_value(MIO_GPIO_GSM_UNKNOWN2, 1);
-	printk("MioA701 phone: GSM turned off.\n");
 }
 
 static void mioa701_phone_configure(int state)
@@ -88,30 +130,21 @@ static void mioa701_phone_configure(int state)
 
 static int mioa701_phone_probe(struct platform_device *dev)
 {
-	/* configure phone GPIOS */
-	pxa_gpio_mode(MIO_GPIO_GSM_OFF | GPIO_OUT | GPIO_DFLT_LOW);
-	pxa_gpio_mode(MIO_GPIO_GSM_PREP | GPIO_OUT);
-	pxa_gpio_mode(MIO_GPIO_GSM_UNKNOWN1 | GPIO_OUT);
-	pxa_gpio_mode(MIO_GPIO_GSM_UNKNOWN2 | GPIO_OUT);
-	pxa_gpio_mode(MIO_GPIO_GSM_EVENT | GPIO_IN);
-
-	/* configure phone UART */
-	pxa_gpio_mode(MIO_GPIO_GSM_RXD_MD);
-	pxa_gpio_mode(MIO_GPIO_GSM_CTS_MD);
-	pxa_gpio_mode(MIO_GPIO_GSM_DCD_MD);
-	pxa_gpio_mode(MIO_GPIO_GSM_DSR_MD);
-	pxa_gpio_mode(MIO_GPIO_GSM_TXD_MD);
-	pxa_gpio_mode(MIO_GPIO_GSM_DTR_MD);
-	pxa_gpio_mode(MIO_GPIO_GSM_RTS_MD);
-
+	phone_init_gpios();
+	ffuart_init_gpios();
 	phone_on();
-	return 0;
+
+	set_irq_type (gpio_to_irq(MIO_GPIO_GSM_MOD_ON_STATE), IRQT_BOTHEDGE);
+	return request_irq(gpio_to_irq(MIO_GPIO_GSM_MOD_ON_STATE), phone_power_irq,
+			  IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING,
+			  "GSM XS200 Power Irq", NULL);
 }
 
 static int
 mioa701_phone_remove(struct platform_device *dev)
 {
 	phone_off();
+	free_irq(gpio_to_irq(MIO_GPIO_GSM_MOD_ON_STATE), NULL);
 	return 0;
 }
 
@@ -135,7 +168,12 @@ static int __init mioa701_phone_init( void )
 	printk(KERN_NOTICE "mioa701 Phone Driver\n");
 	mioa701_phone_funcs.configure = mioa701_phone_configure;
 	pxa_set_ffuart_info(&mioa701_phone_funcs);
-	
+
+	/* If out of windows through Haret, let's do some cleanup */
+	phone_init_gpios();
+	phone_off();
+	phone_reset();
+
 	return platform_driver_register( &phone_driver );
 }
 
