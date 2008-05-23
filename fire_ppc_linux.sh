0aab3109601bd1da70fefa34badef5b99e5d1632
Robert Jarzmik <rjarzmik@free.fr>
2008-05-23 23:03:00 +0200

    Modify tools to economise SD media.
    Change dynamically zImage name to have a rotary zImage.
    Dynamically create haret script to launch the newly created
    kernel on the SD card (partition 1).
    
    Signed-off-by: Robert Jarzmik <rjarzmik@free.fr>

--- fire_ppc_linux.sh
+++ fire_ppc_linux.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
 . /opt3/env.sh
-prun /Linux3/haret.exe
+kernel=`cat /tmp/last_kernel`
+out='/tmp/kernel_file.txt'
+rm $out 2>&1 >/dev/null
+echo -e "set KERNEL $kernel\r\n" >> $out
+echo -e "set MTYPE 1257\r\n" >> $out
+echo -e "set CMDLINE \"debug rootdelay=6 root=/dev/mmcblk0p2 console=tty0 fbcon=rotate:3 mem=64M psplash=false 2\"\r\n" >> $out
+echo -e "boot2\r\n" >> $out
+
+prun "/Storage Card/Linux/haret.exe"
+sleep 2
+nc 169.254.2.1 9999 < $out
 
--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 . /opt3/sync-engine/env.sh
-pcp arch/arm/boot/zImage :/Linux3/zImage
+date=`date +%Y_%m_%d_%Hh%M`
+file="zImage_$date"
+pcp arch/arm/boot/zImage ":/Storage Card/Linux/$file"
+echo "$file" > /tmp/last_kernel
