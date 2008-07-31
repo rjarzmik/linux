df880485847dff33548d86107bf8a6a79185fc2e
Robert Jarzmik <robert.jarzmik@free.fr>
2008-07-31 23:32:20 +0200

    Improved modules_install script to transfer through tar.
    Signed-off-by: Robert Jarzmik <robert.jarzmik@free.fr>

--- modules_install.sh
+++ modules_install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+rm -rf /tmp/kernel_mods_root
+make modules_install INSTALL_MOD_PATH=/tmp/kernel_mods_root
+
+cd /tmp/kernel_mods_root && tar c . | ssh root@mio "cd / && tar xv"
 cd /lib/modules/2.6.25-mioa701/kernel
-scp -r * root@192.168.0.202:/lib/modules/2.6.25-mioa701/kernel/
-#cd /lib/modules/2.6.25-mioa701
-#scp -r modules* root@192.168.0.202:/lib/modules/2.6.25-mioa701/
 
