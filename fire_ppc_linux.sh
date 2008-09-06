fa9b90453741555761d2f94aa2c730e12bcfa1e1
Robert Jarzmik <robert.jarzmik@free.fr>
2008-09-06 12:13:45 +0200

    Scripts: add mioa701 maxfreq parameter of 520MHz to cpufreq
    Signed-off-by: Robert Jarzmik <robert.jarzmik@free.fr>

--- fire_ppc_linux.sh
+++ fire_ppc_linux.sh
@@ -6,7 +6,7 @@ out='/tmp/kernel_file.txt'
 rm $out 2>&1 >/dev/null
 echo -e "set KERNEL $kernel\r\n" >> $out
 echo -e "set MTYPE 1257\r\n" >> $out
-echo -e "set CMDLINE \"debug rootdelay=6 root=/dev/mmcblk0p2 console=tty0 fbcon=rotate:3 mem=64M psplash=false 2\"\r\n" >> $out
+echo -e "set CMDLINE \"debug rootdelay=6 root=/dev/mmcblk0p2 console=tty0 fbcon=rotate:1 mem=64M psplash=false cpu-pxa.pxa27x_maxfreq=520 2\"\r\n" >> $out
 echo -e "boot2\r\n" >> $out
 
 prun "/Storage Card/Linux/haret.exe"
