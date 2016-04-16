92c123ddf9fbf8eb9fbcbde0ce958a888cc15081
Robert Jarzmik <robert.jarzmik@free.fr>
2016-04-16 23:51:14 +0200

    ALSA: ac97: add an ac97 bus
    AC97 is a bus for sound usage. It enables for a AC97 AC-Link to link one
    controller to 0 to 4 AC97 codecs.
    
    The goal of this new implementation is to implement a device/driver
    model for AC97, with an automatic scan of the bus and automatic
    discovery of AC97 codec devices.
    
    Signed-off-by: Robert Jarzmik <robert.jarzmik@free.fr>
    ---
    Since v1:
     - Takashi's review
       - changed the codec.h guard ... a better name could be found ...
       - added the AC97_* macros missing parenthesis
       - constantified the id_table in the codec driver structure
       - changed the 4 codecs linked list into an array
       - enabled the ac97 bus to be a module
       - added a slots_available to snd_ac97_controller_register() to have a
         way to prevent scanning and probing of unconnected codecs
       - removed useless ac97 bus index
       - all exported functions begin with snd_ac97_*()
       - change bus operations to controller+slot parameters instead of
         codec device
    
     - Mark's review
       - changed ac97_digital_controller into ac97_controller
       - rename ac97_digital_controller_*() into ac97_controller_*()
       - add the ac97 ac-link clock to the codec device (ie. the AC'97
         BIT_CLK)
    
    Since v2:
     - more snd_ac97 namespace review
     - change the compat allocation prototype to force the user to provide
       and ac97_codec_device structure pointer

---
+++ sound/ac97/ac97_core.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2016 Robert Jarzmik <robert.jarzmik@free.fr>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+unsigned int snd_ac97_bus_scan_one(struct ac97_controller *ac97,
+				   int codec_num);
